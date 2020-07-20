8aae50caaa21b15c6a6b8644effbd50d87416995
Leo Liu <leoliuhy@gmail.com>
2020-07-20 00:45:45 -0400

    Added calculator V0.cpp

---
+++ Hour2/calculatorV0.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // Take two user inputs, add them together and print out
+    double firstNum, secondNum;
+
+    cout << "Enter your first number -> ";
+    cin >> firstNum;
+    cout << endl << "Enter your second number -> ";
+    cin >> secondNum;
+
+    cout << endl << "The sum of the two numbers that you entered is " << firstNum + secondNum << endl;
+
+
+    return 0;
+}
