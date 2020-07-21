040f994056fd1ed4c3ba97d6de5fde900db0834a
Leo Liu <leoliuhy@gmail.com>
2020-07-21 13:41:30 -0400

    Created WhileLoop.cpp

---
+++ Hour3/WhileLoop.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i = 0;
+    while (i <= 5){
+        cout << i << endl;
+        i++;
+    }
+
+    int b = 10;
+    do{
+        cout << b << endl;
+        b++;
+    }while(b <= 15);
+
+
+    return 0;
+}
