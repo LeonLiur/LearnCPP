1f015c88f8787b9f16be9990488d04f7f66c8a72
Leo Liu <leoliuhy@gmail.com>
2020-07-21 22:49:03 -0400

    Added Comments.cpp

---
+++ Hour3/Comments.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    /* I don't like comments since they take a long time to write and read,
+    but they are sometimes useful for other people to understand your code,
+    because other people are stupid, just kidding, unless...just kidding.
+    Comments in all programming languages are pretty much the same, you
+    write them, but when you loook at your code again after several months, 
+    you still don't understand anything even if it's commented*/
+
+    // Returning from the main funtion - C++ only
+    return 0;
+}
